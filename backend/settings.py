c3464fecede556f530a0ff3e0a3fe5e997287a2c
Олег <osinotov@wejet.ru>
2026-01-10 15:41:47 +0300

    Refactor docker-compose and backend settings; update healthcheck syntax, improve DEBUG handling, and enhance static file serving in URLs.

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG", ""))
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
 ALLOWED_HOSTS = ["*"]
 
@@ -158,7 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "statics/"
+STATIC_URL = "/statics/"
 STATICFILES_DIRS = [
     BASE_DIR / "statics",
 ]
