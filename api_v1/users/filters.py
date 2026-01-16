6717f2b704e3c7637eb1ad72213ac90340f687dc
Evgenia <eug.gross@yandex.ru>
2026-01-16 11:20:48 +0300

    fixed users filters

--- api_v1/users/filters.py
+++ api_v1/users/filters.py
@@ -2,9 +2,8 @@ import django_filters
 from users.models import Candidate
 
 class CandidateFilter(django_filters.FilterSet):
-    vacancy = django_filters.NumberFilter(field_name="vacancy__id")
     position = django_filters.CharFilter(field_name="vacancy__position__name_ru", lookup_expr="icontains")
-    organization = django_filters.NumberFilter(field_name="vacancy__department__organization__id")
+    organization = django_filters.NumberFilter(field_name="vacancy__department__organization")
     status = django_filters.CharFilter(field_name="status")
     department = django_filters.CharFilter(field_name="vacancy__department__name", lookup_expr="icontains")
 
