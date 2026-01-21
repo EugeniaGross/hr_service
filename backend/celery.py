0d2d3ffacd5eced0f9451872ed8e2ae72d1b5fdf
Evgenia <eug.gross@yandex.ru>
2026-01-21 17:32:52 +0300

    fixed user serializers

--- backend/celery.py
+++ backend/celery.py
@@ -2,10 +2,17 @@ import os
 
 from celery import Celery
 from celery.schedules import crontab
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
-app = Celery('backend')
+if settings.DEBUG:
+    os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
+    
+app = Celery(
+    'backend',
+    broker=settings.CELERY_BROKER_URL,
+    backend=settings.CELERY_RESULT_BACKEND,
+)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
