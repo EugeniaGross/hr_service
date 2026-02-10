806a30cd3cad9f5439e8ffd15cf88d39c534d5f7
Evgenia <eug.gross@yandex.ru>
2026-02-10 16:02:40 +0300

    add signature in EC consent

--- api_v1/positions/serializers.py
+++ api_v1/positions/serializers.py
@@ -6,3 +6,13 @@ class PositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Position
         fields = "__all__"
+        
+    def validate(self, attrs):
+        for field in ("name_ru", "name_fr", "name_en"):
+            value = attrs.get(field)
+            if value:
+                if Position.objects.filter(**{field: value}).exists():
+                    raise serializers.ValidationError({
+                        field: "Такое значение уже существует"
+                    })
+        return attrs
--- positions/models.py
+++ positions/models.py
@@ -5,42 +5,22 @@ class Position(models.Model):
     name_ru = models.CharField(
         "Наименование должности (русский)", 
         max_length=255,
-        unique=True,
         blank=True
     )
     name_fr = models.CharField(
         "Наименование должности (французский)", 
         max_length=255,
-        unique=True,
         blank=True
     )
     name_en = models.CharField(
         "Наименование должности (английский)", 
         max_length=255,
-        unique=True,
         blank=True
     )
     
     class Meta:
         verbose_name = "должность"
         verbose_name_plural = "Должности"
-        constraints = [
-            models.UniqueConstraint(
-                fields=["name_ru"],
-                condition=models.Q(name_ru__isnull=False),
-                name="unique_not_null_name_ru"
-            ),
-            models.UniqueConstraint(
-                fields=["name_fr"],
-                condition=models.Q(name_fr__isnull=False),
-                name="unique_not_null_name_fr"
-            ),
-            models.UniqueConstraint(
-                fields=["name_en"],
-                condition=models.Q(name_en__isnull=False),
-                name="unique_not_null_name_en"
-            ),
-        ]
 
     def __str__(self):
         return self.name_ru
