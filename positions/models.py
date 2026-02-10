7122191f2383f894e5a462ed4af5103ea698ad56
Evgenia <eug.gross@yandex.ru>
2026-02-10 13:49:01 +0300

    add signature in EC consent

--- positions/models.py
+++ positions/models.py
@@ -5,17 +5,20 @@ class Position(models.Model):
     name_ru = models.CharField(
         "Наименование должности (русский)", 
         max_length=255,
-        unique=True
+        unique=True,
+        blank=True
     )
     name_fr = models.CharField(
         "Наименование должности (французский)", 
         max_length=255,
-        unique=True
+        unique=True,
+        blank=True
     )
     name_en = models.CharField(
         "Наименование должности (английский)", 
         max_length=255,
-        unique=True
+        unique=True,
+        blank=True
     )
     
     class Meta:
