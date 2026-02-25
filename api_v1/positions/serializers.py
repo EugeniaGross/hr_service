efc2297e52c6434061bdfa542e4cd15ba4ad198e
Олег <osinotov@wejet.ru>
2026-02-25 09:55:52 +0300

    Refactor position name validation in serializer to improve uniqueness checks

--- api_v1/positions/serializers.py
+++ api_v1/positions/serializers.py
@@ -7,12 +7,19 @@ class PositionSerializer(serializers.ModelSerializer):
         model = Position
         fields = "__all__"
         
-    def validate(self, attrs):
-        for field in ("name_ru", "name_fr", "name_en"):
-            value = attrs.get(field)
-            if value:
-                if Position.objects.filter(**{field: value}).exists():
-                    raise serializers.ValidationError({
-                        field: "Такое значение уже существует"
-                    })
-        return attrs
+    def _validate_unique_name(self, field_name, value):
+        qs = Position.objects.filter(**{field_name: value})
+        if self.instance:
+            qs = qs.exclude(pk=self.instance.pk)
+        if qs.exists():
+            raise serializers.ValidationError("Такое значение уже существует.")
+        return value
+
+    def validate_name_ru(self, value):
+        return self._validate_unique_name("name_ru", value)
+
+    def validate_name_fr(self, value):
+        return self._validate_unique_name("name_fr", value)
+
+    def validate_name_en(self, value):
+        return self._validate_unique_name("name_en", value)
